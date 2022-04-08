3f6c7419dd5f750b14df8f27307521e29bb76c0d
oat1655 <oat1655@gmail.com>
2022-04-08 07:55:31 -0700

    create package

---
+++ build/beginner_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nt/LearnRobocupOP/src/beginner_tutorials
+# Build directory: /home/nt/LearnRobocupOP/build/beginner_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/nt/LearnRobocupOP/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/nt/LearnRobocupOP/devel;/home/nt/workspace/devel;/opt/ros/melodic'.split(';') if '/home/nt/LearnRobocupOP/devel;/home/nt/workspace/devel;/opt/ros/melodic' != '' else []
